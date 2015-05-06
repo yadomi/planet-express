81a7a4e2c6379d3884e975eff3bc579be3118cc8
Felix Yadomi <dev.yadomi@gmail.com>
2015-05-07 01:12:27 +0200

    Implemented initialize command

--- lib/planet.rb
+++ lib/planet.rb
@@ -36,23 +36,50 @@ module Planet
     class Application < Thor
 
       PLANET_FILE = 'Planetfile'
+      LOCAL_DIR = File.expand_path('..', __FILE__)
+
+
 
       def initialize(*args)
         super
+
+        unless initialized? then return end
+
         if not File.file?(File.join(Dir.pwd, PLANET_FILE))
-          raise 'No Planetfile found in current folder'
+          abort 'No Planetfile found in current folder'
         end
+
         @@planetfile = File.join(Dir.pwd, PLANET_FILE)
         load @@planetfile
+
       end
+
+
 
       desc 'init', 'Initialize project with a dummy Planetfile and adds deploy scripts'
       def init
+
+        if initialized? then 
+          abort 'This projet has already bean initialized with a Planetfile'
+        end
+
         Dir.mkdir 'deploy/'
+        template_dir = File.join(LOCAL_DIR, 'template')
+        FileUtils.cp(File.join(template_dir, PLANET_FILE), '.')
+        FileUtils.cp(File.join(template_dir, 'after_deploy.sh'), 'deploy/')
+        FileUtils.chmod "u=wrx,go=rx", 'deploy/after_deploy.sh'
+
       end
+
+
 
       desc 'setup NAME', 'Create the remote git repository and install push hooks for it'
       def setup(target)
+
+        unless initialized? then 
+          abort "This project hasn't been initialized. Run planet init to initialize and commit your changes"
+        end
+
         uri = Planet.servers[target.to_sym].uri
         Net::SSH.start(uri.host, uri.user, :keys => [ Planet.configuration.keys ]) do |ssh|
           cmd = %{  
@@ -63,6 +90,8 @@ module Planet
           ssh.exec(cmd)
         end
       end
+
+
 
       desc 'deploy TARGET [BRANCH]', 'Deploy the application to the specified target'
       def deploy(target, branch='master')
@@ -75,6 +104,14 @@ module Planet
           }
           ssh.exec(cmd)
         end
+      end
+
+
+
+      private
+      def initialized?
+        cmd = %x[git ls-files | grep -c '#{PLANET_FILE}' ].chomp!
+        if cmd === '0' then false else true end
       end
 
     end
